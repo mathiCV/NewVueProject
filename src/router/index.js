9fec38f725259c72a59639a59ebcd635fc49d0b4
mathiCV <escorpiowild@gmail.com>
2026-01-13 11:40:09 -0500

    Limpiando el proyecto inicial y configurando router con archivos necesarios

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import LibrosView from '@/views/LibrosView.vue'
+import NuevoLibroView from '@/views/NuevoLibroView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'inicio',
+      component: LibrosView
+    },
+    {
+      path: '/nuevo-libro',
+      name: 'nuevoLibro',
+      component: NuevoLibroView
+    }
+  ],
 })
 
 export default router
