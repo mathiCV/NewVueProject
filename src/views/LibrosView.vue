5c7370ac659809616ce4dfb346315186b8c06542
mathiCV <escorpiowild@gmail.com>
2026-01-13 11:45:08 -0500

    Agregando views requeridos

---
+++ src/views/LibrosView.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+    <h1>Inicio</h1>
+</template>
+
+
+<style scoped>
+
+
+</style>
---
+++ src/views/NuevoLibroView.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+    <h1>Nueo libro</h1>
+</template>
+
+
+<style scoped>
+
+
+</style>
